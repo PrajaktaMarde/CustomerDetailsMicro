564a174553939ca7f1a358b74b6f1e7db90c0205
prajaktamarde <prajakta.marde06@gmail.com>
2022-10-16 20:14:51 +0530

    Unit test

---
+++ CustomerContactDetailsApi.Test/ControllerTests/CustomerContactDetailsTest.cs
@@ -0,0 +1,36 @@
+﻿using CustomerContactDetailsApi.Test.MockData;
+using CustomerContactDetailsAPI.Controllers;
+using CustomerContactDetailsData.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentValidation;
+using CustomerContactDetailsData.Models;
+
+namespace CustomerContactDetailsApi.Test.ControllerTests
+{
+    public class CustomerContactDetailsTest
+    {
+       
+        [Fact]
+        public async Task GetAllAsync_ShouldReturn200Status()
+        {
+            /// Arrange
+            var objUnitofWork = new Mock<IUnitOfWork>();
+            var validator = new Mock<IValidator<CustomerContactDetails>> ();
+            objUnitofWork.Setup(c => c.CustomerContactDetails.GetAll()).ReturnsAsync(CustomerContactMockData.GetCustomerDetails());
+            var sut = new CustomerContactDetailsController(objUnitofWork.Object, validator.Object);
+
+            /// Act
+            var result = (OkObjectResult)await sut.GetCustomerContactDetails();
+
+
+            // /// Assert
+            result.StatusCode.Equals(200);
+        }
+    }
+}
---
+++ CustomerContactDetailsApi.Test/MockData/CustomerContactMockData.cs
@@ -0,0 +1,37 @@
+﻿using CustomerContactDetailsData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerContactDetailsApi.Test.MockData
+{
+    public class CustomerContactMockData
+    {
+        public static List<CustomerContactDetails> GetCustomerDetails()
+        {
+            return new List<CustomerContactDetails>{
+             new CustomerContactDetails{
+                 CustomerId = 1,
+                 CustSocialSecurityNumber = "7894561230",
+                 CustEmailId ="abc@xyz.com",
+                 CustPhoneNumber = "+46789456123"
+
+             },
+              new CustomerContactDetails{
+                 CustomerId = 2,
+                 CustSocialSecurityNumber = "7894561238",
+                 CustEmailId ="abc1@xyz.com",
+                 CustPhoneNumber = "+46789456173"
+
+             },
+              new CustomerContactDetails{
+                 CustomerId = 3,
+                 CustSocialSecurityNumber = "7894561239",
+                 CustEmailId ="abc2@xyz.com",
+                 CustPhoneNumber = "+467894561889"
+
+             },
+         };
+        }
+    }
+}
