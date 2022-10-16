da285759fab4249f6647078b6fb60e34d0d39205
prajaktamarde <prajakta.marde06@gmail.com>
2022-10-16 15:29:53 +0530

    Added Customer details model and db context

---
+++ CustomerContactDetailsData/Data/DbContexts/CustomerContactDetailsContext.cs
@@ -0,0 +1,43 @@
+﻿using CustomerContactDetailsData.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerContactDetailsData.Data.DbContexts
+{
+    public class CustomerContactDetailsContext : DbContext
+    {
+        public CustomerContactDetailsContext(DbContextOptions<CustomerContactDetailsContext> options) : base(options)
+        {
+        }
+        public DbSet<CustomerContactDetails> CustomerContactDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CustomerContactDetails>().HasData(
+                new CustomerContactDetails
+                {
+                    
+                    CustSocialSecurityNumber = "789456989",
+                    CustEmailId = "abc@gmail.com",
+                    CustPhoneNumber = "+46025896389",
+                },
+                new CustomerContactDetails
+                {
+                    
+                    CustSocialSecurityNumber = "789456990",
+                    CustEmailId = "abc1@gmail.com",
+                    CustPhoneNumber = "025894344",
+                },
+                new CustomerContactDetails
+                {
+                   
+                    CustSocialSecurityNumber = "789456991",
+                    CustEmailId = "abc2@gmail.com",
+                    CustPhoneNumber = "+46025896478",
+                }
+            );
+        }
+    }
+}
---
+++ CustomerContactDetailsData/Data/Models/CostomerContactDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CustomerContactDetailsData.Data.Models
+{
+    public class CustomerContactDetails
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        public string CustSocialSecurityNumber { get; set; }
+
+        public string CustEmailId { get; set; }
+
+        public string CustPhoneNumber { get; set; }
+    }
+}
